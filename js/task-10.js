16408ae49bedc63dcfcf5aa1dca4f25496314246
yashbon <104573777+yashbon@users.noreply.github.com>
2023-03-27 10:39:16 +0300

    completed task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const buttonCreate = document.querySelector('button[data-create]')
+const boxesDiv = document.querySelector('#boxes')
+
+buttonCreate.addEventListener('click', onClick)
+
+function onClick(event) {
+  const qty = document.querySelector('input[type ="number"]').value
+
+  let width = 30,
+    height = 30,
+    subBoxHtml = ''
+
+  if (qty) {
+
+    for (let i = 1; i <= qty; i++) {
+      const colorSet = getRandomHexColor();
+
+      subBoxHtml += `<div style="width: ${width}px; height: ${height}px; background:${colorSet}">&nbsp;</div>`;
+      //boxesDiv = boxesDiv.innerHTML + subBoxHtml;
+
+      width += 10
+      height += 10
+    }
+    boxesDiv.innerHTML = subBoxHtml
+  }
+  //console.log(qty)
+
+
+}
