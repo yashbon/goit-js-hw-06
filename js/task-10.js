eb9f71ca59b6b8da0d8a64dd729205a0ad933724
yashbon <104573777+yashbon@users.noreply.github.com>
2023-03-27 11:32:57 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,33 +4,39 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-
-const buttonCreate = document.querySelector('button[data-create]')
+const buttonCreate = document.querySelector('[data-create]')
 const boxesDiv = document.querySelector('#boxes')
+const buttonDestroy = document.querySelector('[data-destroy]')
 
-buttonCreate.addEventListener('click', onClick)
+buttonCreate.addEventListener('click', onClickCreate)
+buttonDestroy.addEventListener('click', onClickDestroy)
 
-function onClick(event) {
-  const qty = document.querySelector('input[type ="number"]').value
 
-  let width = 30,
-    height = 30,
-    subBoxHtml = ''
+function onClickCreate(event) {
+  const amount = document.querySelector('[type ="number"]').value
 
-  if (qty) {
+  createBoxes(amount)
 
-    for (let i = 1; i <= qty; i++) {
+}
+
+function onClickDestroy(event) {
+  boxesDiv.innerHTML = ''
+  document.querySelector('[type ="number"]').value = ''
+}
+
+function createBoxes(amount) {
+  let width = 30;
+  let height = 30;
+  let subBoxHtml = ''
+  if (amount) {
+    for (let i = 1; i <= amount; i++) {
       const colorSet = getRandomHexColor();
 
       subBoxHtml += `<div style="width: ${width}px; height: ${height}px; background:${colorSet}">&nbsp;</div>`;
-      //boxesDiv = boxesDiv.innerHTML + subBoxHtml;
 
       width += 10
       height += 10
     }
     boxesDiv.innerHTML = subBoxHtml
   }
-  //console.log(qty)
-
-
 }
