b92dbed383e61537aa6ef6d93cd9a9f13c35a20d
yashbon <104573777+yashbon@users.noreply.github.com>
2023-03-27 10:38:43 +0300

    completed task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector('.login-form')
+const submit = document.querySelector('button[type=submit]')
+
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(event) {
+
+    const userEmail = event.currentTarget.email.value;
+    const userPassword = event.currentTarget.password.value;
+
+    if (!userEmail || !userPassword) {
+        alert('всі поля повинні бути заповнені')
+    } else {
+        const userForm = {
+            email: '',
+            password: ''
+        }
+        userForm.email = userEmail;
+        userForm.password = userPassword;
+
+        // Output to console
+        console.log(userForm);
+
+        // reset
+        event.currentTarget.reset();
+    }
+    // form does not reload
+    event.preventDefault()
+}
+
 
