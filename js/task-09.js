2fa1d828a3bebc15c8ac12118ef243f120ada9fb
yashbon <104573777+yashbon@users.noreply.github.com>
2023-03-27 10:38:59 +0300

    completed task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorOutput = document.querySelector('.color')
+const changeColorButton = document.querySelector('.change-color')
+
+changeColorButton.addEventListener('click', onClick)
+
+function onClick(event) {
+  const colorSet = getRandomHexColor();
+  colorOutput.innerHTML = colorSet;
+  document.querySelector('body').style = `background-color: ${colorSet}`
+}
